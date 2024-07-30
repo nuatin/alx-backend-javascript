3455f9c9a67569dba24649eadef0934d7c0328dc
nuatin <areyourorlah@gmail.com>
2024-07-30 17:20:58 +0000

    0x03-ES6_data_manipulation#

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+*creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author nuatin ayoola <https://github.com/nuatin>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
---
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -0,0 +1,11 @@
+/**
+*checks if a set contains each element in an array.
+ * @param {Set} set - The collection of unique items.
+ * @param {*} array - The array of items.
+ * @author nuatin ayoola <https://github.com/nuatin>
+ * @returns {Boolean}
+ */
+export default function hasValuesFromArray(set, array) {
+  return array.every((value) => set.has(value));
+}
+
