18996cdcb7ac23dcf64d03e7f0902b7ad50ebc4b
nuatin <areyourorlah@gmail.com>
2024-09-21 22:40:11 +0000

    0x05-Node_JS_basic

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Ayoola Nuatin <https://github.com/nuatin>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
